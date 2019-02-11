6dbf8b659e9380093f32501ddc2b8ca5943f43c9
Sumeet Bansal <sumeetbansal@gmail.com>
2019-02-11 10:42:12 -0800

    added HW2 #3 in PHP

---
+++ envecho.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>Big Droplet Energy</title>
+<link rel="shortcut icon" href="/favicon.ico?" type="image/x-icon"/>
+</head>
+
+<?php
+echo "<strong>\$_SERVER</strong>:<br>";
+print_r ($_SERVER);
+echo "<br><br><strong>\$_REQUEST</strong>:<br>";
+print_r ($_REQUEST);
+?>
+
+</body>
+</html>
