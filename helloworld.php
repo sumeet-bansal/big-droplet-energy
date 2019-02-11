bec62b4c952299e0029292fa589813f4d5966e62
Sumeet Bansal <sumeetbansal@gmail.com>
2019-02-10 16:06:35 -0800

    added HW2 #1 in PHP

---
+++ helloworld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Big Droplet Energy</title>
+	<link rel="shortcut icon" href="/favicon.ico?" type="image/x-icon"/>
+</head>
+
+<?php
+$cols = array('blue', 'yellow', 'white');
+$page_color = $cols[rand(0, 2)];
+echo "<body style='background-color: " . $page_color . "'>";
+echo "Hello Web World from Language PHP on " . date("h:i:sa") . " enjoy my " . $page_color . "page!";
+?>
+
+</body>
+</html>
