2e153e3155770c2e4ea1cf9752851e59d9c530d0
Sumeet Bansal <sumeetbansal@gmail.com>
2019-02-13 18:56:48 -0800

    added missing </head> in form.php

--- form.php
+++ form.php
@@ -1,33 +1,30 @@
 <html>
-    <head>
-        <script type="text/javascript">
-            function changeMethod() {
-                var meth = document.getElementById("water");
-                document.getElementById("main").method = meth.options[meth.selectedIndex].value;
-            }
-        </script>
-    
+<head>
+    <script type="text/javascript">
+        function changeMethod() {
+            var meth = document.getElementById("water");
+            document.getElementById("main").method = meth.options[meth.selectedIndex].value;
+        }
+    </script>
+</head>
 <body>
 
-<form id="main" action="/echo.php" method="get">
-    first name: <input type="text" name="fname"><br>
-    last name: <input type="text" name="lname"><br>
-    fave color: <select name="color">
-        <option value="">select</option>
-        <option value="blue">blue</option>
-        <option value="yellow">yellow</option>
-        <option value="white">white</option>
-    </select>
-    method: <select id="water" name="method">
-        <option value="">select</option>
-        <option value="get">GET</option>
-        <option value="post">POST</option>
-    </select>
-    <input type="submit" value="submit" onclick="changeMethod()">
-</form>
-
-
+    <form id="main" action="/echo.php" method="get">
+        first name: <input type="text" name="fname"><br>
+        last name: <input type="text" name="lname"><br>
+        fave color: <select name="color">
+            <option value="">select</option>
+            <option value="blue">blue</option>
+            <option value="yellow">yellow</option>
+            <option value="white">white</option>
+        </select>
+        method: <select id="water" name="method">
+            <option value="">select</option>
+            <option value="get">GET</option>
+            <option value="post">POST</option>
+        </select>
+        <input type="submit" value="submit" onclick="changeMethod()">
+    </form>
 
 </body>
-
 </html>
